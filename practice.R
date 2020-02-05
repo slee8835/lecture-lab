759517cd229fe18c48d9cf0e4275a9cafd9af789
Sharon Lee <slee8835@gmail.com>
2020-02-04 22:12:47 -0800

    second commit

--- practice.R
+++ practice.R
@@ -7,15 +7,23 @@ sum(num)
 #1. Use the function c to create a vector with the average high temperatures in January for
 #Beijing, Lagos, Paris, Rio de Janeiro, San Juan, and Toronto, which are 35, 88, 42, 84, 81, 
 #and 30 degrees Fahrenheit. Call the object temp.
+temp <- c(35, 88, 42, 84, 30)
 
 # Now create a vector with the city names and call the object city.
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
 
-# Use the names function and the objects defined in the previous exercises to associate the temperature data with its corresponding city.
+# Use the names function and the objects defined in the previous exercises to associate 
+#the temperature data with its corresponding city.
+names(city) <- temp
 
-# Use the [ and : operators to access the temperature of the first three cities on the list.
-             
+# Use the [ and : operators to access the temperature of the first three cities on the 
+#list.
+city[1:3]
+
 # Use the [ operator to access the temperature of Paris and San Juan.
-                          
+city[3]
+city[5]
+
 # Use the : operator to create a sequence of numbers  12,13,14,.....73
                         
                           
