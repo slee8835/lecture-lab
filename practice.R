4678d95b21b8d46e4598b06f4ae914b9443dd342
Sharon Lee <slee8835@gmail.com>
2020-02-11 19:42:17 -0800

    R practice

--- practice.R
+++ practice.R
@@ -7,7 +7,7 @@ sum(num)
 #1. Use the function c to create a vector with the average high temperatures in January for
 #Beijing, Lagos, Paris, Rio de Janeiro, San Juan, and Toronto, which are 35, 88, 42, 84, 81, 
 #and 30 degrees Fahrenheit. Call the object temp.
-temp <- c(35, 88, 42, 84, 30)
+temp <- c(35, 88, 42, 84, 81, 30)
 
 # Now create a vector with the city names and call the object city.
 city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
@@ -15,20 +15,30 @@ city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
 # Use the names function and the objects defined in the previous exercises to associate 
 #the temperature data with its corresponding city.
 names(city) <- temp
-
+city_temp <- data.frame(city, temp)
 # Use the [ and : operators to access the temperature of the first three cities on the 
 #list.
 city[1:3]
-
+city_temp$temp[1:3]
 # Use the [ operator to access the temperature of Paris and San Juan.
 city[3]
 city[5]
+city_temp$temp[3]
+city_temp$temp[5]
 
 # Use the : operator to create a sequence of numbers  12,13,14,.....73
-                        
+seq_num <- 12:73    
                           
 # Create a vector containing all the positive odd numbers smaller than 100.
-                          
+odd_vec <- 0
+for(i in 0: 100){
+  if(i %% 2 == 0){
+    #print(i)
+    append(odd_vec,i)    
+  }
+}
+odd_vec
+i 
 # Create a vector of numbers that starts at 6, does not pass 55, and adds numbers in increments of 4/7: 6, 6 + 4/7, 6 + 8/7, and so on. How many numbers does the list have? Hint: use seq and length.
                           
 # What is the class of the following object a <- seq(1, 10, 0.5)?
